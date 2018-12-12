04cbef92f1b701b665f0124ef94b8631246607db
Joel Mathews <jmathews@CHI-EN-361G8WL.attlocal.net>
2018-12-12 01:57:42 -0600

    Initial Commit

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import request
+import requests
+import pymongo
+from pymongo import MongoClient
+
+
+
+
+client = MongoClient()
+app = Flask(__name__)
+
+@app.route('/', methods=[ 'POST'])
+def hello_world():
+    if flask.request.method == 'POST':
+      return 'POST'
+    return 'Hj'
+
+@app.route('/yolo', methods=['GET', 'POST'])
+def add_message():
+    db = client.testDB
+    content = request.json
+    print (content['yo'])
+    posts = db.posts
+    post_data = {
+        'content':content['yo']
+    }
+    result = posts.insert_one(post_data)
+    return "hello"
